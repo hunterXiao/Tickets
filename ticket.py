9f378fcb4a2c3004fe126a804dde9cae62843e73
hunterShaw <hunter.m.xiao@gmail.com>
2014-10-28 22:38:15 +0800

    .
    .

--- ticket.py
+++ ticket.py
@@ -6,12 +6,14 @@ ticket.py
 """
 import  os
 import  os.path
+import  urllib
 
 import  httplib2
 
+http = httplib2.Http(cache= '.cache', disable_ssl_certificate_validation= True)
 
 #Global variables
-stations = []
+stations = None
 
 seatTypeCodes = [
     ('1', u'硬座'), 
@@ -33,8 +35,23 @@ cardTypeCodes = [
 ]
 
 
-#----------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# 火车站点数据加载
+# 全部站点, 数据来自: https://kyfw.12306.cn/otn/resources/js/framework/station_name.js
+# 每个站的格式如下:
+# @bji|北京|BJP|beijing|bj|2   ---> @拼音缩写三位|站点名称|编码|拼音|拼音缩写|序号
+
 def stationLoad():
-    """Load station info from ./station file and append into global variables stations"""
-    
+    try:
+        f = open('./station')
+        sta_str = f.read()
+    except IOError, e:
+        return (0, u'站点信息文件[station]读取失败！')
+    stalist = [x.split('|') for x in sta_str.strip().split('@')]
+    stations = [{'abbr': sta[0], 'name': sta[1], 'telecode': sta[2], 'pinyin': sta[3], 'pyabbr': sta[4]} for sta in stalist]
+    for i in  stations:
+        print i
+
+if __name__ == '__main__':
+    stationLoad()
     
